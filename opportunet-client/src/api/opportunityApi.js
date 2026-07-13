d48f4fdcad2a5bfd3653b50117488668cfba77a3
Abishek <abishekabishek5891@gmail.com>
2026-07-13 10:37:54 +0530

    Use environment variable for API URL to support production deploy

--- opportunet-client/src/api/opportunityApi.js
+++ opportunet-client/src/api/opportunityApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/opportunities';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/opportunities';
 
 export const getOpportunities = async (filters = {}) => {
   const response = await axios.get(API_URL, { params: filters });
